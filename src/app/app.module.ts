ace7255df30c4d1c842c126471fcdad7142f0ae9
Grace Stuart <gstuart@gmail.com>
2017-05-12 15:01:01 -0700

    add mentor component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,14 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { routing } from './app.routing';
 import { ContactComponent } from './contact/contact.component';
-
+import { MentorComponent } from './mentor/mentor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ContactComponent,
-  
+    MentorComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,7 +2,7 @@ import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
-
+import { MentorComponent } from './mentor/mentor.component';
 
 const appRoutes: Routes = [
   {
@@ -16,6 +16,10 @@ const appRoutes: Routes = [
   {
     path: 'user',
     component: UserComponent
+  },
+  {
+    path: 'mentor',
+    component: MentorComponent
   }
 ];
 
