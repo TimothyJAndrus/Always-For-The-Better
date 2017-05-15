62227447482b582e9e142a47b2524a7f9c98eda9
Grace Stuart <gstuart@gmail.com>
2017-05-15 07:33:22 -0700

    display edit form

--- src/app/edit-user/edit-user.component.ts
+++ src/app/edit-user/edit-user.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { User } from '../user.model';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-edit-user',
   templateUrl: './edit-user.component.html',
-  styleUrls: ['./edit-user.component.css']
+  styleUrls: ['./edit-user.component.css'],
+  providers: [UserService]
 })
 export class EditUserComponent implements OnInit {
+  @Input() selectedUser;
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
   }
 
+  // beginUpdatedUser(userToUpdate) {
+  //   this.userService.updateUser(userToUpdate);
+  // }
 }
